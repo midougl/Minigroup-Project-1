ccd5a42ee97e2d12aaf0a67ae6892b1ee1936609
brianbeach95 <42755236+brianbeach95@users.noreply.github.com>
2022-02-17 10:43:16 -0600

    Update randomFile.h
    Added comments at the top

--- randomFile.h
+++ randomFile.h
@@ -1,3 +1,13 @@
+/**
+ * Group A
+ * Brian Beach
+ * 2/16/22
+ * randomFile.c or randomFile.h Description:
+ * Returns a string that is in the form of "input_xx.txt" where xx is a random digit from 1-10
+ * Used to get the input.txt file for the game. Made into a .h file so the game can call it when it begins.
+ * 
+ **/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,7 +36,7 @@ char* randomInputFile() {
     strcpy(fileName, temp);
 
     return fileName;
-
+    
 }
 
 
