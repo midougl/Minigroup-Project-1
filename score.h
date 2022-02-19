cc0a62628251199db939e6997dad40c2d730467f
Samuel Fipps <Samuelfipps@gmail.com>
2022-02-19 04:17:55 -0600

    Update score.h

--- score.h
+++ score.h
@@ -4,37 +4,22 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-typedef struct{
-    int score;
-}player_build;
-player_build (**player);
-
-
 
 int TotalScore = 0;
 int player1 = 0;
 int num_players =5;
-
+int score[3];
+int playerTacker=0;
 
 int Calcscore(int wordSize, bool FoundInDic){
 
-    player = (player_build **) malloc(num_players*sizeof(player_build*));
-    for (int i =0; i<num_players; i++){
-        player[i] = (player_build*) malloc(num_players * sizeof(player_build));
-    }
+    if(wordSize<5)         score[playerTacker] = score[playerTacker] + 1;
+    else if(wordSize == 5) score[playerTacker] = score[playerTacker] + 2;
+    else if(wordSize == 6) score[playerTacker] = score[playerTacker] + 3;
+    else if(wordSize == 7) score[playerTacker] = score[playerTacker] + 5;
+    else if(wordSize >= 8) score[playerTacker] = score[playerTacker] + 11;
 
-    for(int i =0; i< num_players; i++){
-        (player[i])->score=0;
-    }
-
-
-    if(wordSize<5)printf("1");
-    else if(wordSize == 5) player1 = player1 + 2;
-    else if(wordSize == 6) player1 = player1 + 3;
-    else if(wordSize == 7) player1 = player1 + 5;
-    else if(wordSize >= 8) player1 = player1 + 11;
-
-    if(FoundInDic) player1= player1 +5;
+    if(FoundInDic) score[playerTacker] = score[playerTacker] +5;
 
     return 0;
 }
