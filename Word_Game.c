5235e5eb5ff82b4a51fad225b289d1689787a1f2
Samuel Fipps <Samuelfipps@gmail.com>
2022-02-19 04:16:02 -0600

    Update Word_Game.c

--- Word_Game.c
+++ Word_Game.c
@@ -1,22 +1,3 @@
-/**
- * Group A
- * Brian Beach / Samuel Fipps
- * 2/16/22
- * 
- * Word_Game.c  Description:
- * Brian - made a game that took input file x (randomly generated from randomFile.h) and has the player create a word from those letters
- * The game then determines if the word is valid (checks if only given letters were used). If so, adds score and goes to next part where player
- * needs to make the word from the ending of the previously used word. Goes until the player messes up
- * 
- * Samuel - turned parts of the game into seperate functions (availableLetters, endOfWord) so that the game can be changed up when we create
- * a single_player and multi_player options. Added score.h to be able to keep correct scoring system.
- * 
- * Still things needed to be done -ex: create array to keep previously used words, turn word_game into two seperate games for single player and 
- * multi player
- **/
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,137 +7,209 @@
 #include "dictionary_Check.h"
 #include "score.h"
 
-bool availableLetters(char *userInput, char *playText);
-bool endOfWord(char *userInput, char *newInput);
+bool availableLetters();
+bool endOfWord();
+bool check_dict();
+void gameCheckers();
+void getWordFromTxt();
+void menu();
+void randCharPicker();
 
-    /****************************************************************************************************************************************
-     * Need part to check for single and multiplayer (probably use for loop for how ever many players with array for score)
-     * Probably need to fix scoring system (inprogress)
-     * NEEDS TO COMPARE TO DICTIONAIRY (done)
-     * need to make array to keep list of words previously used
-     * etc
-    ****************************************************************************************************************************************/
+
+char userInput[32];
+int playTextLength = 255;
+char playText[255];
+char newInput[32];
+FILE* inputFile;
+int lengthOfWord =0;
+int menuOp;
+char startingChar;
 
 int main() {
-    bool cont = true;
-    int lengthOfWord =0;
-    bool foundInDic = false;
 
+    menu();  // calls menu function
+    getWordFromTxt();  //gets word from txt file
 
-    //used to read input file
-    FILE* inputFile;
-    int playTextLength = 255;
-    char playText[playTextLength];
+    if(menuOp==1){
+        gameCheckers();    // starts game
+    }
+    if(menuOp==2){
+        printf("not setup yet");
+    }
 
+}
+
+void menu(){
+    printf("\n1.    Single Player Mode\n");
+    printf("2.    Multi-Player Mode\n");
+    printf("3.    Exit\n \n");
+    scanf("%d", &menuOp);
+
+    if(menuOp == 1) printf("Starting Single Player Mode\n \n");
+    if(menuOp == 2) printf("Starting Multi-Player Mode\n \n");
+    if(menuOp == 3) printf("Exiting");
+
+    if(menuOp==3){
+        exit(0);
+    }
+}
+
+void getWordFromTxt(){
     //gets random input file in form "input_xx.txt"
     char *file = randomInputFile();
-    printf("%s\n", file);
+    printf("Using game file: %s\n", file);
     inputFile = fopen(file, "r");
 
     //gets word to play from input file and inputs it into playText
-    fgets(playText, playTextLength, inputFile);
-    printf("%s\n", playText);       //test to see if working
+    fgets(userInput, playTextLength, inputFile);
     fclose(inputFile);
+}
 
-    //for first created word
-    printf("Make a word from %s\n", playText);
-    char userInput[32];
-    scanf("%s", userInput);
-
-    // main game loop
-    while (cont){ // main game loop
-    cont = availableLetters(userInput, playText);
-    //checks if userInput is in dictionary.txt
-    if (dictionary(userInput) == 0) {
-        printf("%s is not accepted in the given dictionary\n", userInput);
-        printf("Make a word from %s\n", playText);
-        cont = false;
-        foundInDic = true;
-        break;
-    }
-
-    //score stuff
+void randCharPicker(){  // pick ranndom char at start of the game and random player
+    int ranChar =0;
+    char charHolder;
+    int lengthOfWord =0;
+    //char test[32];
+    bool one = true;
+    srand(time(0));
     lengthOfWord = strlen(userInput);
+    ranChar = (rand() % lengthOfWord-1) ;//random cahr
+    charHolder = userInput[ranChar];
+    ranChar = (rand() % 2)+1 ; // random player
+    playerTacker = ranChar;
+    printf("starting player is %d \n", playerTacker);
+
+    //starting the game here
+    printf("Make a word from %c\n", charHolder);
+    while(one){
+        scanf("%s", newInput);
+        one = availableLetters();   // makes sure letter user inputed are in the word
+        if(one) check_dict();   //checks if userInput is in dictionary.txt
+    }
+    strcpy(userInput, newInput);   //sets up for next round
+    lengthOfWord = strlen(userInput);  //score
     Calcscore(lengthOfWord , foundInDic);
     foundInDic = false;
-
-
-        //continuing off the ending of the previous word
-    printf("Make a word using the ending character(s) of %s\n", userInput);
-    char newInput[32];
-    scanf("%s", newInput);
-    if(strlen(newInput)<3){
-        printf("word to small needs to be atleast 3 chars");
-        break;
-    }
-    //makes sure its the end of the previous word
-    cont = endOfWord(userInput, newInput);
-    strcpy(userInput, newInput);
-    }
-
 }
 
-
-bool availableLetters(char *userInput, char *playText){
+void gameCheckers(){
     bool cont = true;
-    //int score=0;
+    bool one = true;
+    bool two = true;
+    bool three = true;
+    int twoPasses =0;
+    char pass[4]= {'p','a','s','s'};
+
+    printf("Your set of alphabets is %s\n", userInput);
+    randCharPicker(); //starts game
+
+    while(cont){
+
+        if(playerTacker==1)playerTacker=2;
+        else playerTacker=1;
+        printf("It is now player %d turn\n", playerTacker);
+
+        printf("Make a word from %s\n", userInput);
+        scanf("%s", newInput);
+
+        for(int i=0; i <4; i++){// for passing
+            if(newInput[i]==pass[i])
+            printf("passing to other player\n");
+            i = 10;
+            twoPasses++;
+        }
+
+        if(twoPasses==2){// if both players pass
+            printf("both players have passed reseting\n");
+            randCharPicker();
+        }
+
+        if(twoPasses==0){//if no one passes
+
+            two = check_dict();             //checks if userInput is in dictionary.txt
+            if(two) three = endOfWord();        //makes sure its the end of the previous word
+
+
+            if(one && two && three){
+                strcpy(userInput, newInput);   //sets up for next round
+
+                lengthOfWord = strlen(userInput);  //score
+                Calcscore(lengthOfWord , foundInDic);
+                foundInDic = false;
+
+            }
+        }
+        else if (twoPasses!=1)twoPasses =0;
+    }
+}
+
+bool check_dict(){
+    if (dictionary(newInput) == 0) {
+        printf("%s is not accepted in the given dictionary\n", newInput);
+    }
+    else{
+        foundInDic = true;
+        return true;
+    }
+return false;
+}
+
+bool availableLetters(){
     int check;
 
-    for (int i = 0; i < strlen(userInput); ++i) {
-        check = 0;
-        for (int j = 0; j < strlen(playText); ++j) {
-            if (tolower(userInput[i]) == tolower(playText[j])) {
-                check = 1;
-            }
-        }
-        if (check == 0) {
-            printf("Word does not count because you used %c which is not in the list of letters\n", userInput[i]);
-            //score = 0;
-            cont = false;
-            break;
-            }
-        else {
-            cont = true;
+    check = 0;
 
+    for (int j = 0; j < strlen(userInput); ++j) {
+        if (tolower(newInput[0]) == tolower(userInput[j])) {
+            check = 1;
         }
     }
-    return cont;
+    if (check == 0) {
+        printf("Word does not count because you used '%c' which is not in the list of letters\n", newInput[0]);
+        return true;
+        }
+    else {
+        return false;
+
+    }
+
+return true;
 }
 
-
-bool endOfWord(char *userInput, char *newInput){  // ill clean this up later
-    int j =0;
-    char one[10];
-    char two[20];
+bool endOfWord(){
     int count = 0;
-    char hold[1];
-    char hold2[1];
-    bool end = false;
+    char word1[100];
+    bool sameWord = false;
+    count = 0;
+    sameWord=false;
 
-   // int lentgh = strlen(userInput);
     for(int i=0; i<strlen(userInput); i++){
-        if(end) return true;
-        j--;
-        hold2[0] = userInput[strlen(userInput)+j];
-        one[i] = userInput[strlen(userInput)+j];
-        two[i] = (newInput[i]);
-        hold[0] = two[0];
-        count ++;
-        if(hold[0] == hold2[0]) {
-        printf("here");
-            for(int k =0; k<count; k++){
-                end = true;
-                if(two[k]!= one[count-k-1]){
-                    return false;
+        if(newInput[i]== userInput[i] ){
+            count++;
+        }
+    }
+
+    if(count == strlen(userInput)){
+        printf("You can't use the same word\n");
+        sameWord = true;
+    }
+
+    if(sameWord==false){
+        for(int i=0; i < strlen(userInput); i++){
+            word1[i] = userInput[strlen(userInput)-i-1];
+            count =0;
+            for(int j=0; j<strlen(userInput)+1; j++){
+                if( (word1[  i-j ])   ==  (newInput[j]) ){
+                    count++;
+                }
+                if(count == i+1){
+                    return true;
                 }
             }
         }
-
     }
-    return false;
-
+return false;
 }
-
 
 
 
