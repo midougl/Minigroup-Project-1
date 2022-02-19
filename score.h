1b99fa9a44a6d034dc0da796d255319d4395b946
Samuel Fipps <Samuelfipps@gmail.com>
2022-02-18 21:42:54 -0600

    Update score.h

--- score.h
+++ score.h
@@ -4,9 +4,30 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+typedef struct{
+    int score;
+}player_build;
+player_build (**player);
+
+
+
 int TotalScore = 0;
 int player1 = 0;
+int num_players =5;
+
+
 int Calcscore(int wordSize, bool FoundInDic){
+
+    player = (player_build **) malloc(num_players*sizeof(player_build*));
+    for (int i =0; i<num_players; i++){
+        player[i] = (player_build*) malloc(num_players * sizeof(player_build));
+    }
+
+    for(int i =0; i< num_players; i++){
+        (player[i])->score=0;
+    }
+
+
     if(wordSize<5)printf("1");
     else if(wordSize == 5) player1 = player1 + 2;
     else if(wordSize == 6) player1 = player1 + 3;
@@ -14,4 +35,6 @@ int Calcscore(int wordSize, bool FoundInDic){
     else if(wordSize >= 8) player1 = player1 + 11;
 
     if(FoundInDic) player1= player1 +5;
+
+    return 0;
 }
